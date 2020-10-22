4809b0376aa5c4f1b5cad35bc381d89019dfccd9
Jordan Cassem <cassem.codes@gmail.com>
2020-10-22 15:04:21 +0100

    Connect to dynamodb

--- restApi/service/go.mod
+++ restApi/service/go.mod
@@ -1,3 +1,5 @@
 module service.com/property
 
 go 1.15
+
+require github.com/aws/aws-sdk-go v1.35.12
--- restApi/service/propertyService.go
+++ restApi/service/propertyService.go
@@ -1,28 +1,128 @@
 package property
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
+)
+
 // Property model
 type Property struct {
+	ID   string  `json:"id"`
 	Name string  `json:"name"`
 	Rent float32 `json:"rent"`
 }
 
-// Example slice
-var properties = []Property{
-	Property{
-		Name: "123 Fake Street",
-		Rent: 1200.00,
-	},
-	Property{
-		Name: "2 Main Road",
-		Rent: 899.50,
-	},
-	Property{
-		Name: "Flat A 120 Regents Street",
-		Rent: 14060.66,
-	},
+var svc (*dynamodb.DynamoDB)
+var tableName = "props"
+
+// init Create dynamo db session
+func new() {
+	if svc == nil {
+		// Initialize a session that the SDK will use to load
+		// credentials from the shared credentials file ~/.aws/credentials
+		// and region from the shared configuration file ~/.aws/config.
+		sess := session.Must(session.NewSessionWithOptions(session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}))
+
+		// Create DynamoDB client
+		svc = dynamodb.New(sess)
+	}
+
+	listTablesTest()
+}
+
+func listTablesTest() {
+	input := &dynamodb.ListTablesInput{}
+	fmt.Printf("Tables:\n")
+
+	for {
+		// Get the list of tables
+		result, err := svc.ListTables(input)
+		if err != nil {
+			if aerr, ok := err.(awserr.Error); ok {
+				switch aerr.Code() {
+				case dynamodb.ErrCodeInternalServerError:
+					fmt.Println(dynamodb.ErrCodeInternalServerError, aerr.Error())
+				default:
+					fmt.Println(aerr.Error())
+				}
+			} else {
+				// Print the error, cast err to awserr.Error to get the Code and
+				// Message from an error.
+				fmt.Println(err.Error())
+			}
+			return
+		}
+
+		for _, n := range result.TableNames {
+			fmt.Println(*n)
+		}
+
+		// assign the last read tablename as the start for our next call to the ListTables function
+		// the maximum number of table names returned in a call is 100 (default), which requires us to make
+		// multiple calls to the ListTables function to retrieve all table names
+		input.ExclusiveStartTableName = result.LastEvaluatedTableName
+
+		if result.LastEvaluatedTableName == nil {
+			break
+		}
+	}
 }
 
 // ListProperties Lists all properties
 func ListProperties() []Property {
-	return properties
+	fmt.Printf("List Properties\n")
+	return getItemsFromDynamodb()
+}
+
+func getItemsFromDynamodb() []Property {
+	new()
+
+	// Create the Expression to fill the input struct with.
+	// Get all properties with name
+	filt := expression.Name("name").Equal(expression.Value("123 Fake Street"))
+
+	// Get specific fields
+	proj := expression.NamesList(expression.Name("ID"), expression.Name("Name"), expression.Name("Rent"))
+
+	expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
+
+	if err != nil {
+		fmt.Println("Got error building expression:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	// Build the query input parameters
+	params := &dynamodb.ScanInput{
+		ExpressionAttributeNames:  expr.Names(),
+		ExpressionAttributeValues: expr.Values(),
+		FilterExpression:          expr.Filter(),
+		ProjectionExpression:      expr.Projection(),
+		TableName:                 aws.String(tableName),
+	}
+
+	propertyList := []Property{}
+
+	// Make the DynamoDB Query API call
+	result, err := svc.Scan(params)
+	if err != nil {
+		fmt.Println("Query API call failed:")
+		fmt.Println((err.Error()))
+		os.Exit(1)
+	}
+	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &propertyList)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	return propertyList
 }
