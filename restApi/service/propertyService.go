b283e47a0b8238de4175871dbde598501712d4e0
Jordan Cassem <cassem.codes@gmail.com>
2020-10-24 17:56:12 +0100

    Remove query parameters to return all results

--- restApi/service/propertyService.go
+++ restApi/service/propertyService.go
@@ -7,7 +7,6 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
-	"github.com/aws/aws-sdk-go/service/dynamodb/expression"
 )
 
 // Property model
@@ -17,45 +16,25 @@ type Property struct {
 	Rent float32 `json:"rent"`
 }
 
+// tableName DynamoDb table name to query against
 const tableName = "props"
 
 // ListProperties Lists all properties
 func ListProperties(dynamoDbSession *dynamodb.DynamoDB) []Property {
 	fmt.Printf("List Properties\n")
-
-	// Create the Expression to fill the input struct with.
-	// Get all properties with name
-	filt := expression.Name("name").Equal(expression.Value("123 Fake Street"))
-
-	// Get specific fields
-	proj := expression.NamesList(expression.Name("ID"), expression.Name("Name"), expression.Name("Rent"))
-
-	expr, err := expression.NewBuilder().WithFilter(filt).WithProjection(proj).Build()
-
-	if err != nil {
-		fmt.Println("Got error building expression:")
-		fmt.Println(err.Error())
-		os.Exit(1)
-	}
-
-	// Build the query input parameters
-	params := &dynamodb.ScanInput{
-		ExpressionAttributeNames:  expr.Names(),
-		ExpressionAttributeValues: expr.Values(),
-		FilterExpression:          expr.Filter(),
-		ProjectionExpression:      expr.Projection(),
-		TableName:                 aws.String(tableName),
-	}
-
 	propertyList := []Property{}
 
-	// Make the DynamoDB Query API call
-	result, err := dynamoDbSession.Scan(params)
+	queryParams := &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	}
+
+	result, err := dynamoDbSession.Scan(queryParams)
 	if err != nil {
 		fmt.Println("Query API call failed:")
 		fmt.Println((err.Error()))
 		os.Exit(1)
 	}
+
 	err = dynamodbattribute.UnmarshalListOfMaps(result.Items, &propertyList)
 	if err != nil {
 		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
